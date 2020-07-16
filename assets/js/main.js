0814df618a23a9850f971f5af4b694da1b72362d
eduardsrcs <itbeard@inbox.lv>
2020-07-16 22:04:25 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+let buttons = document.querySelectorAll('.mybtns')
+let windows = document.querySelectorAll('.windows')
+
+buttons.forEach((el, index) => makeSome(el, index))
+windows.forEach((el, index) => el.dataset.num = index)
+
+setInterval(()=> makeTime('time'), 1000)
+
+const makeTime = el => {
+	let t = new Date()
+	let h = t.getHours()
+	let m = t.getMinutes()
+	let s = t.getSeconds()
+	let e = document.getElementById(el)
+	if (e) e.innerHTML = `${nn(h)}:${nn(m)}:${nn(s)}`
+}
+
+const nn = a => a < 10 ? `0${a}` : a
+
+function makeSome(el, index){
+	// el.innerHTML = `Button ${index+1}`
+	el.setAttribute('data-num', index)
+	el.addEventListener('click', () => showWindow(el.dataset.num))
+}
+
+function resetwindows(e){
+	showWindow(null)
+}
+
+function showWindow(num){
+	windows.forEach((el, index) => {
+		 num == el.dataset.num
+			? el.classList.toggle('winwis')
+			: el.classList.remove('winwis')
+	})
+}
